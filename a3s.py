655509c3b3084d3e70343f574d39ccfef1ef1025
Markus Schneider <markus@icet-clan.de>
2016-08-23 18:50:00 +0000

    Fixed parameters

--- a3s.py
+++ a3s.py
@@ -5,9 +5,12 @@ import argparse
 def main():
     # Command line argument setup
     parser = argparse.ArgumentParser(description="Arma 3 Repository Updater")
-    parser.add_argument("-a", "--add", help="Add mod to repository")
-    parser.add_argument("-r", "--remove", help="Remove mod from repository")
-    parser.add_argument("-u", "--update", help="Update repository")
+    parser.add_argument("-a", "--add", action="store_true",
+                        help="Add mod to repository")
+    parser.add_argument("-r", "--remove", action="store_true",
+                        help="Remove mod from repository")
+    parser.add_argument("-u", "--update", action="store_true",
+                        help="Update repository")
 
     args = parser.parse_args()
 
