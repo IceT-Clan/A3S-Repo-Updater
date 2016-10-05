97d79a3fbc425a322ef3cb3b9fed64bfefec8205
Nikolai Zimmermann <Admiral.Chronophylos@web.de>
2016-10-05 15:17:50 +0200

    splittet a function into two

--- console.py
+++ console.py
@@ -11,30 +11,55 @@ class Output:
         self.is_debug = is_debug
         self.ansi_escape = EscapeAnsi.EscapeAnsi()
 
-    def printstatus(self, state, displayname="NO DISPLAY NAME", **kwargs):
+    def printerrstate(self, state, kwargs):
+        """print a error status with information for the user"""
+        if state == "err_not_valid":
+            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
+                             + self.ansi_escape.RESET + " ] "
+                             + kwargs["file_name"] + " is not a valid "
+                             + kwargs["file_format"] + "\n")
+        elif state == "err_not_exist":
+            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
+                             + self.ansi_escape.RESET + " ] "
+                             + kwargs["displayname"]
+                             + " does not exist" + "\n")
+        elif state == "err_steam":
+            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
+                             + self.ansi_escape.RESET + " ] "
+                             + "Maybe SteamCMD did not download "
+                             + kwargs["displayname"]
+                             + "(" + kwargs["workshop_id"] + ") correctly? "
+                             + "(use --steam-only to skip other sources)"
+                             + "\n")
+        return
+
+    def printstatus(self, state, **kwargs):
         """print a status with information for the user"""
         state = str(state).lower()
         if state == "updating":
             # Updating
             sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_YELLOW
                              + "WAIT" + self.ansi_escape.RESET + " ] "
-                             + "Updating " + displayname + "...")
+                             + "Updating " + kwargs["displayname"] + "...")
         elif state == "is_up_to_date":
             # Is up to date
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_GREEN + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
-                             + " is up to date" + "\n")
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"] + " is up to date" + "\n")
         elif state == "success_update":
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_GREEN + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"]
                              + " successfully updated" + "\n")
         elif state == "steambag_add":
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_BLUE + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"]
                              + " has been added to the Steam Bag" + "\n")
         elif state == "ace_opt_add":
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_BLUE + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"]
                              + " will be added to @ace_optinals" + "\n")
         elif state == "do_workshop":
             sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_YELLOW + "WAIT"
@@ -43,35 +68,24 @@ class Output:
         elif state == "linking":
             sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_YELLOW + "WAIT"
                              + self.ansi_escape.RESET + " ] "
-                             + "linking " + displayname + "...")
+                             + "linking " + kwargs["displayname"] + "...")
         elif state == "success_linking":
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_GREEN + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"]
                              + " successfully linked" + "\n")
         elif state == "is_linked":
             sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_BLUE + "SKIP"
-                             + self.ansi_escape.RESET + " ] " + displayname
+                             + self.ansi_escape.RESET + " ] "
+                             + kwargs["displayname"]
                              + " is already linked" + "\n")
         elif state == "success_removed":
             sys.stdout.write("\r" + "[  " + self.ansi_escape.F_L_GREEN + "OK"
-                             + self.ansi_escape.RESET + "  ] " + displayname
+                             + self.ansi_escape.RESET + "  ] "
+                             + kwargs["displayname"]
                              + " successfully removed" + "\n")
-        elif state == "err_not_valid":
-            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
-                             + self.ansi_escape.RESET + " ] "
-                             + kwargs["file_name"] + " is not a valid "
-                             + kwargs["file_format"] + "\n")
-        elif state == "err_not_exist":
-            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
-                             + self.ansi_escape.RESET + " ] " + displayname
-                             + " does not exist" + "\n")
-        elif state == "err_steam":
-            sys.stdout.write("\r" + "[ " + self.ansi_escape.F_L_RED + "FAIL"
-                             + self.ansi_escape.RESET + " ] "
-                             + "Maybe SteamCMD did not download " + displayname
-                             + "(" + kwargs["workshop_id"] + ") correctly? "
-                             + "(use --steam-only to skip other sources)"
-                             + "\n")
+        elif state.startswith("err"):
+            self.printerrstate(state, kwargs)
         else:
             raise TypeError(state + " is not a valid state")
 
