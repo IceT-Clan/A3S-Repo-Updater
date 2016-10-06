c0a3c4ba513666ab7c1b05697e062fee3ed90282
Nikolai Zimmermann <Admiral.Chronophylos@web.de>
2016-10-06 15:32:51 +0200

    fixing progress bar

--- misc.py
+++ misc.py
@@ -11,7 +11,8 @@ from tqdm import tqdm
 def download(output, url, file_name, new_line=False):
     """download <URL> to <file_name>"""
     output.debug("download " + url + " as " + file_name, add_newline=new_line)
-    file_size = urllib2.urlopen(url).headers["Content-Length"]
+    #file_size = urllib2.urlopen(url).headers["Content-Length"]
+    file_size = requests.head(url).headers.get('content-length', None)
     with open(file_name, "wb") as download_file:
         response = get(url, stream=True)
         with open('output.bin', 'wb') as output:
