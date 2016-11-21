3043381271f0f85430f9dfb5266bb4a7128aebe0
Nikolai Zimmermann <nikolai.zimmermann@web.de>
2016-11-21 17:30:39 +0100

    reformattet output to console
    Tags are surrounded by '[' and ']' with a additional space between and after to set the messages on the same indent

--- console.py
+++ console.py
@@ -15,17 +15,17 @@ class Output:
         """print a error status with information for the user"""
         if state == "err_not_valid":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_RED + "FAIL" +
-                             ansi_escape.RESET + " ] " +
+                             ansi_escape.RESET + " ]  " +
                              args[0] + " is not a valid " +
                              args[1] + "\n")
         elif state == "err_not_exist":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_RED + "FAIL" +
-                             ansi_escape.RESET + " ] " +
+                             ansi_escape.RESET + " ]  " +
                              args[0] +
                              " does not exist" + "\n")
         elif state == "err_steam":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_RED + "FAIL" +
-                             ansi_escape.RESET + " ] " +
+                             ansi_escape.RESET + " ]  " +
                              "Maybe SteamCMD did not download " +
                              args[0] +
                              "(" + args[1] + ") correctly? " +
@@ -33,7 +33,7 @@ class Output:
                              "\n")
         elif state == "err_skip":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_RED + "SKIP" +
-                             ansi_escape.RESET + " ] " +
+                             ansi_escape.RESET + " ]  " +
                              "Error updating " + args[0] + "\n")
         else:
             raise TypeError(state + " is not a valid state")
@@ -44,49 +44,49 @@ class Output:
         if state == "updating":
             # Updating
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_YELLOW +
-                             "WAIT" + ansi_escape.RESET + " ] " +
+                             "WAIT" + ansi_escape.RESET + " ]  " +
                              "Updating " + args[0] + "...")
         elif state == "is_up_to_date":
             # Is up to date
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_GREEN + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_GREEN + "OK" +
+                             ansi_escape.RESET + " ]    " +
                              args[0] + " is up to date" + "\n")
         elif state == "success_update":
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_GREEN + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_GREEN + "OK" +
+                             ansi_escape.RESET + " ]    " +
                              args[0] +
                              " successfully updated" + "\n")
         elif state == "steambag_add":
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_BLUE + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_BLUE + "OK" +
+                             ansi_escape.RESET + " ]   " +
                              args[0] +
                              " has been added to the Steam Bag" + "\n")
         elif state == "ace_opt_add":
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_BLUE + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_BLUE + "OK" +
+                             ansi_escape.RESET + " ]    " +
                              args[0] +
                              " will be added to @ace_optinals" + "\n")
         elif state == "do_workshop":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_YELLOW +
-                             "WAIT" + ansi_escape.RESET + " ] " +
+                             "WAIT" + ansi_escape.RESET + " ]  " +
                              "doing Steam Workshop..." + "\n")
         elif state == "linking":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_YELLOW +
-                             "WAIT" + ansi_escape.RESET + " ] "
+                             "WAIT" + ansi_escape.RESET + " ]  "
                              "linking " + args[0] + "...")
         elif state == "success_linking":
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_GREEN + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_GREEN + "OK" +
+                             ansi_escape.RESET + " ]    " +
                              args[0] +
                              " successfully linked" + "\n")
         elif state == "is_linked":
             sys.stdout.write("\r" + "[ " + ansi_escape.F_L_BLUE + "SKIP" +
-                             ansi_escape.RESET + " ] " +
+                             ansi_escape.RESET + " ]  " +
                              args[0] +
                              " is already linked" + "\n")
         elif state == "success_removed":
-            sys.stdout.write("\r" + "[  " + ansi_escape.F_L_GREEN + "OK" +
-                             ansi_escape.RESET + "  ] " +
+            sys.stdout.write("\r" + "[ " + ansi_escape.F_L_GREEN + "OK" +
+                             ansi_escape.RESET + " ]    " +
                              args[0] +
                              " successfully removed" + "\n")
         elif state.startswith("err"):
@@ -101,5 +101,5 @@ class Output:
         else:
             newline = ""
         if self.is_debug:
-            sys.stdout.write(newline + "[" + ansi_escape.BOLD + "DEBUG" +
+            sys.stdout.write(newline + "[ " + ansi_escape.BOLD + "DEBUG" +
                              ansi_escape.RESET + " ] " + str(msg) + "\n")
