51d3a38a2c0bc33cec5daa7f899718fd378817b3
Nikolai Zimmermann <nikolai.zimmermann@web.de>
2016-11-24 15:14:13 +0100

    fixed read_config() shouldn't add newlines or empty lines to mod

--- misc.py
+++ misc.py
@@ -30,12 +30,14 @@ def read_config(repo):
     modlist = list()
     with open(repo, "r") as conf:
         for line in conf:
+            if not line:
+                continue
             if line.startswith("#"):
                 continue
-            if line.startswith(" "):
-                continue
-            if line is "":
-                continue
+            #if line.startswith(" "):
+            #    continue
+            #if line is "":
+            #    continue
             modlist.append(line.strip("\n").split(","))
     return modlist
 
